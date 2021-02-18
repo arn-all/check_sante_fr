e6524062c6f024e4ac4d365ab76e41f8c09424db
arn-all <arnaud.allera@univ-lyon1.fr>
2021-02-18 10:16:51 +0100

    Handling bad request response (multiple attempts + notification)

--- check.py
+++ check.py
@@ -1,6 +1,9 @@
 import requests
 import datetime
 import telegram_send
+import logging
+from pprint import pprint
+import time 
 
 today = datetime.datetime.now().date().isoformat()
 centres = [{'place':'Montélimar',
@@ -10,13 +13,32 @@ centres = [{'place':'Montélimar',
         'request_url':'https://partners.doctolib.fr/availabilities.json?start_date={}&visit_motive_ids=2547500&agenda_ids=410324-410327&insurance_sector=public&practice_ids=164487&destroy_temporary=true&limit=7&allowNewPatients=true&telehealth=false&profileId=242529&isOrganization=true&telehealthFeatureEnabled=false&vaccinationMotive=true&vaccinationDaysRange=26&vaccinationCenter=true&nbConfirmedVaccinationAppointments=4881',
         'link_url':"https://partners.doctolib.fr/hopital-public/valence/centre-de-vaccination-du-centre-hospitalier-de-valence?speciality_id=5494&enable_cookies_consent=1"}
         ] 
+max_attempts = 3
 
 for centre in centres:
-    data = requests.get(url=centre['request_url'].format(today)).json()
+    attempt = 0
+    while(attempt < max_attempts):
+        try:
+            response = requests.get(url=centre['request_url'].format(today))
+            data = response.json()
+            pprint(data)        
+            if (int(data["total"])!=0):
+                telegram_send.send(messages=["🔔💉 Créneau(x) disponible(s) à {}. Plus d'info: {}".format(centre['place'], centre['link_url'])], 
+                                    disable_web_page_preview=True)
+        except:
+            attempt+=1
+            time.sleep(60)
+        else:
+            break
+    if attempt == max_attempts:
+        telegram_send.send(messages=["❌ Une erreur s'est produite lors de la vérification des disponibilités à {} ({} tentatives).".format(centre['place'], max_attempts), 
+                                     "💻 `systemctl --user status check.service` pour en savoir plus."], 
+                                     parse_mode="markdown")
+        try:
+            print(response)
+        except:
+            pass
 
-    if (len(data["availabilities"])!=0) | (int(data["total"])!=0):
-        telegram_send.send(messages=["🔔💉 Créneau(x) disponible(s) à {}. Plus d'info: {}".format(centre['place'], centre['link_url'])], 
-                            disable_web_page_preview=True)
     # else:
     #     telegram_send.send(messages=["🙅‍♂️💉 Pas de créneaux à {}. Plus d'info: {}".format(centre['place'], centre['link_url'])], 
     #                         disable_web_page_preview=True)
